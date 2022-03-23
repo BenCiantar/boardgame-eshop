ab7d1373e51ef93d9144046a7944a79ad5d0fe2e
Ben Ciantar <benjamin.ciantar@hyperisland.se>
2022-03-23 14:56:42 +0100

    Added structural elements for search and filter bar, imported React Icons

--- client/src/components/ItemList/ItemList.js
+++ client/src/components/ItemList/ItemList.js
@@ -1,4 +1,6 @@
 import { renderItemList } from "../../scripts/rendering";
+import { FaSistrix } from "react-icons/fa";
+import { GoSettings } from "react-icons/go";
 
 function ItemList(props) {
     let rows = renderItemList(props);
@@ -8,18 +10,25 @@ function ItemList(props) {
             <div id="filter-options">
                 <div id="sort-and-filter">
                     <div id="filter-button">
-                        FB
+                        <button type="submit" value="submit"><GoSettings /></button>
                     </div>
                     <div id="sort">
-                        Sort By
+                        <select name="sorting" id="sorting">
+                            <option value="" selected disabled hidden>Sort by:</option>
+                            <option value="Release-new-old">Release - New to old</option>
+                            <option value="Release-old-new">Release - Old to new</option>
+                            <option value="Price-high-low">Price - High to low</option>
+                            <option value="Price-low-high">Price - Low to high</option>
+                        </select>
                     </div>
                 </div>
                 <div id="search">
                     <div id="search-bar">
-                        Search
+                        <label htmlFor="search"></label>
+                        <input type="text" placeholder="Search items" name="search" required />
                     </div>
                     <div id="search-button">
-                        SB
+                        <button type="submit" value="submit"><FaSistrix /></button>
                     </div>
                 </div>
             </div>
--- client/src/scripts/rendering.js
+++ client/src/scripts/rendering.js
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import { FaShoppingCart } from "react-icons/fa";
 
 
 //-----------Home Page
@@ -17,10 +18,7 @@ export function renderItemList(props) {
                 </div>
                 <div className="item-purchase">
                     <button className="buy-btn btn-in-stock">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="13" height="13" fill="currentColor" className="bi bi-cart-plus-fill" viewBox="0 0 16 16">
-                            <path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1H.5zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0zM9 5.5V7h1.5a.5.5 0 0 1 0 1H9v1.5a.5.5 0 0 1-1 0V8H6.5a.5.5 0 0 1 0-1H8V5.5a.5.5 0 0 1 1 0z"/>
-                        </svg>
-                        {props.items[i].price} kr
+                        <FaShoppingCart /> {props.items[i].price} kr
                     </button>
                 </div>
             </div>
