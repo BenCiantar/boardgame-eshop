8f6f5df4e07db407460836fc6bad651abc5dfd81
Ben Ciantar <benjamin.ciantar@hyperisland.se>
2022-04-05 19:03:24 +0200

    Updated CORS to allow from all sources, not just local host

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ const orderCollection = db.collection('orders');
 const PORT = process.env.PORT || 8080;
 const app = express();
 
-app.use(cors({origin: "http://localhost:3000"}));
+app.use(cors({origin: "*"}));
 app.use(express.json());
 
 //Logging
