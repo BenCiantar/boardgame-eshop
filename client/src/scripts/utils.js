c77118d66ff9b8aa44bec9e6d6a7d2252a675bb9
Ben Ciantar <benjamin.ciantar@hyperisland.se>
2022-04-05 18:56:13 +0200

    Fixed warnings in utils.js

--- client/src/scripts/utils.js
+++ client/src/scripts/utils.js
@@ -1,21 +1,21 @@
 export function sortItems(data, sortBy) {
     let unsortedItems = [...data];
     let sortedItems = [];
-    if (sortBy == "Price-high-low") {
+    if (sortBy === "Price-high-low") {
         sortedItems = unsortedItems.sort((a, b) => { return b.price - a.price; });
-    } else if (sortBy == "Price-low-high") {
+    } else if (sortBy === "Price-low-high") {
         sortedItems = unsortedItems.sort((a, b) => { return a.price - b.price; });
-    } else if (sortBy == "Release-new-old") {
+    } else if (sortBy === "Release-new-old") {
         sortedItems = unsortedItems.sort((a, b) => { return new Date(b.releaseDate) - new Date(a.releaseDate); });
-    } else if (sortBy == "Release-old-new") {
+    } else if (sortBy === "Release-old-new") {
         sortedItems = unsortedItems.sort((a, b) => { return new Date(a.releaseDate) - new Date(b.releaseDate); });
-    } else if (sortBy == "Alpha-a-z") {
+    } else if (sortBy === "Alpha-a-z") {
         sortedItems = unsortedItems.sort((a, b) => {
             if(a.title < b.title) { return -1; }
             if(a.title > b.title) { return 1; }
             return 0;
         });
-    } else if (sortBy == "Alpha-z-a") {
+    } else if (sortBy === "Alpha-z-a") {
         sortedItems = unsortedItems.sort((a, b) => {
             if(a.title > b.title) { return -1; }
             if(a.title < b.title) { return 1; }
